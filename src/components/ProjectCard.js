68adfce6e627af403e87f64c211851f4f8d4df7f
Michael Gaudreau <gaudreaum01@gmail.com>
2021-06-21 15:48:40 -0400

    Create ProjectCard component

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import styled from 'styled-components';
+
+
+export default function ProjectCard() {
+    return (
+        <CardContainer>
+
+            <Card>
+            
+                <CardActionArea>
+                    <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    image="https://loremflickr.com/640/360"
+                    title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                    Share
+                    </Button>
+                    <Button size="small" color="primary">
+                    Learn More
+                    </Button>
+                </CardActions>
+                
+            </Card>
+
+        </CardContainer>
+    )
+}
+
+const CardContainer = styled.div` 
+    width: 20%;
+    background-color: #0000007d;
+    padding: 1rem;
+`
