f999a51f50a45da87a6f2adf42c7f256ade07bbd
Michael Gaudreau <gaudreaum01@gmail.com>
2021-06-23 21:45:13 -0400

    Add slide animation to ProjectCard component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,49 +7,52 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import Slide from 'react-reveal/Slide';
 import styled from 'styled-components';
 
 
 export default function ProjectCard() {
     return (
         <CardContainer>
-
-            <Card>
-            
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    alt="Contemplative Reptile"
-                    image="https://loremflickr.com/640/360"
-                    title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                    Share
-                    </Button>
-                    <Button size="small" color="primary">
-                    Learn More
-                    </Button>
-                </CardActions>
+            <Slide left>
+                <Card>
                 
-            </Card>
+                    <CardActionArea>
+                        <CardMedia
+                        component="img"
+                        alt="Contemplative Reptile"
+                        image="https://loremflickr.com/640/360"
+                        title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Lizard
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                        Share
+                        </Button>
+                        <Button size="small" color="primary">
+                        Learn More
+                        </Button>
+                    </CardActions>
+                    
+                </Card>
+            </Slide>
+
 
         </CardContainer>
     )
 }
 
 const CardContainer = styled.div` 
-    width: 20%;
+    width: 100%;
     background-color: #0000007d;
     padding: 1rem;
 `
